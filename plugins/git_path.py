b402ec9746e67319367a42ab9761a1f13013496a
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2025-04-09 12:15:49 +0530

    Create git_path.py

---
+++ plugins/git_path.py
@@ -0,0 +1,31 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from config import Config
+from globals import AuthU, settings
+from Func.utils import mention_user
+
+# sturcure:
+#    {user_id}/{user_path}
+
+user_paths={}
+defalt="files"
+
+def get_path(id):
+  if not str(id) in user_paths:
+    Fp = defalt
+  else:
+    Fp = user_paths[str(id)]
+  return f"{id}/{Fp}/"
+
+@Client.on_message(filters.command("gitpath"))
+async def st_git_p(client,message):
+  npath = message.text.split(" ")[1]
+  if not is_authorized(message.from.id):
+    await message.reply
+    return
+  user_paths[str(message.from.id)] = npath
+  await message.reply(f"✅️**Now path is:** {getpath(message.from.id)}")
+
+
+  
